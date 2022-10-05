74887b7885d0556c735b187a6fbf5dc10081ee98
shar <Sasha.sharai1998@mail.ru>
2022-10-05 23:02:22 +0300

    add apps "user" in project

---
+++ accounter/user/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import CustomUser
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id','email','first_name','last_name')
+    list_display_links = ('id','email','first_name','last_name')
+    list_filter = ('id','email','is_staff')
+    search_fields = ('email','first_name','last_name')
+    ordering = ('email',)
+
+admin.site.register(CustomUser,CustomUserAdmin)
